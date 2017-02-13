c00e28d0a7bf8b20b608b9d716e5eca23ef4d6e3
Coy Sanders <coymeetsworld@gmail.com>
2017-02-13 13:34:19 -0800

    Organized adding contents in map via functions. Easier to comment out when working to generate maps

--- app/store/configureStore.jsx
+++ app/store/configureStore.jsx
@@ -10,6 +10,85 @@ export var configure = () => {
 		dungeon: dungeonMapReducer
 	});
 	
+	let placeWeapons = () => {
+		//Place weapons
+		defaultMap[5][5] = {
+			...defaultMap[5][5],
+			containsWeapon: true,
+			weapon: {
+				name: 'Club',
+				attack: 10
+			}
+		}
+		defaultMap[12][18] = {
+			...defaultMap[12][18],
+			containsWeapon: true,
+			weapon: {
+				name: 'Dagger',
+				attack: 15
+			}
+		}
+	}	
+	
+	let placeWalls = () => {
+		//Draw some walls
+		for (let i = 3; i < 10; i++) {
+			defaultMap[i][10] = {
+				...defaultMap[i][10],
+				isWall: true	
+			}
+		}	
+		for (let i = 3; i < 10; i++) {
+			defaultMap[10][i] = {
+				...defaultMap[10][i],
+				isWall: true	
+			}
+		}
+	}
+	
+	let placePotions = () => {
+		//Add potions
+		defaultMap[3][9] = {
+			...defaultMap[3][9],
+			containsPotion: true,
+		}
+	}
+	
+	let placeMonsters = () => {
+		//Create a monster
+		defaultMap[0][5] = {
+			...defaultMap[0][5],
+			containsMonster: true,
+			monster: {
+				hp: 25,
+				strength: 5,
+				level: 1,
+				xp: 10 /* derive xp later based on stats of monster, for now simplify it by giving it as an attribute. */
+			}
+		}
+
+		//Create a monster
+		defaultMap[5][12] = {
+			...defaultMap[0][5],
+			containsMonster: true,
+			monster: {
+				hp: 25,
+				strength: 5,
+				level: 1,
+				xp: 10 /* derive xp later based on stats of monster, for now simplify it by giving it as an attribute. */
+			}
+		}
+	}
+	
+	let placeCharacter = (character) => {
+		//Place character
+		defaultMap[0][0] = {
+			...defaultMap[0][0],
+			containsCharacter: true,
+		}
+	
+	}
+	
 	let defaultMap = [MAP_DIMENSIONS_COLUMNS];
 	
 	for (let i = 0; i < MAP_DIMENSIONS_ROWS; i++) {
@@ -25,69 +104,11 @@ export var configure = () => {
 		}
 	}
 
+	placeWeapons();
+	placeWalls();
+	placeMonsters();
+	placePotions();
 
-	//Place weapons
-	defaultMap[5][5] = {
-		...defaultMap[5][5],
-		containsWeapon: true,
-		weapon: {
-			name: 'Club',
-			attack: 10
-		}
-	}
-	defaultMap[12][18] = {
-		...defaultMap[12][18],
-		containsWeapon: true,
-		weapon: {
-			name: 'Dagger',
-			attack: 15
-		}
-	}
-
-	//Draw some walls
-	for (let i = 3; i < 10; i++) {
-		defaultMap[i][10] = {
-			...defaultMap[i][10],
-			isWall: true	
-		}
-	}	
-	for (let i = 3; i < 10; i++) {
-		defaultMap[10][i] = {
-			...defaultMap[10][i],
-			isWall: true	
-		}
-	}
-
-	//Create a monster
-	defaultMap[0][5] = {
-		...defaultMap[0][5],
-		containsMonster: true,
-		monster: {
-			hp: 25,
-			strength: 5,
-			level: 1,
-			xp: 10 /* derive xp later based on stats of monster, for now simplify it by giving it as an attribute. */
-		}
-	}
-
-	//Create a monster
-	defaultMap[5][12] = {
-		...defaultMap[0][5],
-		containsMonster: true,
-		monster: {
-			hp: 25,
-			strength: 5,
-			level: 1,
-			xp: 10 /* derive xp later based on stats of monster, for now simplify it by giving it as an attribute. */
-		}
-	}
-	
-	
-	//Add potions
-	defaultMap[3][9] = {
-		...defaultMap[3][9],
-		containsPotion: true,
-	}
 	
 	//Faro, Jin
 	let defaultCharacter = {
@@ -99,12 +120,9 @@ export var configure = () => {
 		hp: 125,
 		xp: 0
 	}
+	
+	placeCharacter(defaultCharacter);
 
-	//Place character
-	defaultMap[0][0] = {
-		...defaultMap[0][0],
-		containsCharacter: true,
-	}
 	//xp to level should be constant and function defined elsewhere
 	/*weapons an object with strength parameter?
 	const weapons = {
