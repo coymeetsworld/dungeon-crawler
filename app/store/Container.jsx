5b1787cf85dafa516a522c14f9eeeba664dd46d9
Coy Sanders <coymeetsworld@gmail.com>
2017-02-13 17:55:52 -0800

    Added feature to bound container sizes to a predefined size or larger

--- app/store/Container.jsx
+++ app/store/Container.jsx
@@ -4,6 +4,9 @@
 */
 import {BSPTree} from 'BSPTree';
 
+const HORZ_RATIO = 0.45;
+const VERT_RATIO = 0.45;
+
 export class Point {
 	constructor(x,y) {
 		this.x = x,
@@ -24,13 +27,22 @@ export let randomSplit = (container) => {
 		//Create a vertical container
 		c1 = new Container( container.x, container.y, random(1, container.width), container.height);
 		c2 = new Container( container.x + c1.width, container.y, container.width - c1.width, container.height);
+
+		if (c1.width/c1.height < VERT_RATIO || c2.width/c2.height < VERT_RATIO) {
+			return randomSplit(container); // Discards c1 and c2
+		}
 		
 	}	else {
 		//Create a horizontal container
 		c1 = new Container( container.x, container.y, container.width, random(1, container.height));
 		c2 = new Container( container.x, container.y + c1.height, container.width, container.height - c1.height);
+
+		if (c1.height/c1.width < HORZ_RATIO || c2.height/c2.width < HORZ_RATIO) {
+			return randomSplit(container); // Discards c1 and c2
+		}
 		
 	}
+	
 	return [c1, c2];
 }
 
